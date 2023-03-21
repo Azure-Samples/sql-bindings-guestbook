2e06badd7c6229d393abc249c4e85a38f562a4f0
Drew Skwiers-Koballa <drew.koballa@gmail.com>
2023-03-21 07:36:07 -0700

    add web apis

---
+++ apis/web-api/AddEntry.cs
@@ -0,0 +1,37 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Azure.Samples
+{
+    public static class AddEntry
+    {
+        [FunctionName("AddEntry")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "Entry")] HttpRequest req,
+            [Sql("app.Entry", "SqlConnectionString")] IAsyncCollector<Entry> newEntries,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            string newEntryText = data?.newEntry;
+
+            Entry newEntry = new Entry();
+            newEntry.TextEntry = newEntryText;
+            newEntry.DateCreated = DateTime.Now;
+            newEntry.DisableView = false;
+
+            await newEntries.AddAsync(newEntry);
+
+            return new OkObjectResult(newEntry);
+        }
+    }
+}
---
+++ apis/web-api/Entry.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Azure.Samples
+{
+    public class Entry
+    {
+        public int? Id { get; set; }
+        public bool DisableView { get; set; }
+        public string TextEntry { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime? DateModerated { get; set; }
+    }
+}
